25b06b41b756cf063b6ea83b0be15213347b3dd4
Austin_macbook <austincanter@yahoo.com>
2024-08-01 23:01:20 -0500

    cleaning up visuals

--- src/HP.gd
+++ src/HP.gd
@@ -8,7 +8,7 @@ func _ready():
 	pass # Replace with function body.
 
 func take_damage() -> void:
-	hp_tracker = hp_tracker - 10
+	hp_tracker = hp_tracker - 50
 	var value = "HP: %s" % hp_tracker
 	hp.text = value
 
